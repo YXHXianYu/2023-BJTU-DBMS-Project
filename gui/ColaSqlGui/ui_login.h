cff031d72a196076d028b37ba9bf3c8b4bdbf2c6
FuLiqi <1832271620@qq.com>
2023-04-20 16:21:47 +0800

    fix:修复了.ignore会ignore头文件的bug

---
+++ gui/ColaSqlGui/ui_login.h
@@ -0,0 +1,38 @@
+#ifndef UI_LOGIN_H
+#define UI_LOGIN_H
+
+#include <ui_register.h>
+
+#include <QMainWindow>
+#include <QMessageBox>
+
+#include "dataprocessor.h"
+
+namespace Ui {
+class ui_login;
+}
+
+class ui_login : public QMainWindow {
+  Q_OBJECT
+
+ public:
+  explicit ui_login(QWidget *parent = nullptr);
+  ~ui_login();
+
+ signals:
+  void login();
+
+ private slots:
+  void on_pushButton_login_clicked();
+
+  void on_pushButton_regist_clicked();
+
+ private:
+  Ui::ui_login *ui;
+  ui_register *ui_regist;
+  QString user_name;
+  QString user_pwd;
+  void clear();
+};
+
+#endif  // UI_LOGIN_H
