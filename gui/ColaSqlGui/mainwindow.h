2a87137a77e8462e6f87e72e85bf2699a7e4d673
FuLiqi <1832271620@qq.com>
2023-05-17 17:27:20 +0800

    feat(gui): drop database&table&field

--- gui/ColaSqlGui/mainwindow.h
+++ gui/ColaSqlGui/mainwindow.h
@@ -37,42 +37,68 @@ public:
     ~MainWindow();
 
 private:
-    QString anyToQString(const std::any& value);
-    std::string anytoString(const std::any& value);
-    void display_table(std::vector<std::vector<std::any>>& return_records);
-    int use_database(std::string database);
-    int select_all_from_table(
-        std::string tbName, std::vector<std::vector<std::any>>& return_records);
-    void cancel_change();
-    void save_change();
+    // 辅助功能
+    QString anyToQString(const std::any& value);    // any转QString
+    std::string anytoString(const std::any& value); // any转string
+    int use_database(std::string database);         // 使用数据库
+    int select_all_from_table(std::string tbName,
+                              std::vector<std::vector<std::any>>&
+                                  return_records); // select * from table
+    void display_table(
+        std::vector<std::vector<std::any>>& return_records); // 展示查询结果
+
+    void cancel_change(); // 取消单点修改
+    void save_change();   // 保存单点修改
 
 private slots:
-    void click_action_database();
-    void click_action_table();
-    void click_action_column();
-    void click_action_row();
-    void save();
+    // 点击事件
+    void click_create_database();                        // 创建库
+    void click_create_table();                           // 创建表
+    void click_create_field();                           // 创建字段
+    void click_create_record();                          // 创建记录
+    void click_delete_db();                              // 删除数据库
+    void click_delete_tb();                              // 删除表
+    void click_delete_field();                           // 删除字段
+    void click_delete_record();                          // 删除记录
+    void click_save();                                   // 保存文件
+    void on_btn_commit_clicked();                        // commit
+    void on_btn_rollback_clicked();                      // rollback
+    void on_tableView_clicked(const QModelIndex& index); // 点击表
+
+    // 实现功能
+    void create_table(QString, QString); // 创建表
+    void create_record(QString);         // 创建一条记录
+    void handleTableModified();          // 点击修改某一记录
+
+    // 树形结构
+    void init_treeview(); // 初始化树形结构
+    void on_treeView_doubleClicked(const QModelIndex& index); // 双击树形结构
+
+    // override命令行回车键
     void onEnterPressed();
-    void init_treeview();
-    void on_treeView_doubleClicked(const QModelIndex& index);
-    void create_table(QString, QString);
-    void create_record(QString);
-    void handleTableModified();
-    void on_btn_commit_clicked();
-    void on_btn_rollback_clicked();
 
 protected:
+    // override命令行回车键
     bool eventFilter(QObject* watched, QEvent* event) override;
 
 private:
     Ui::MainWindow* ui;
+    // 登录界面
     ui_login* ui_log;
+    // 新建表界面
     createtable* ui_create_table;
+    // 新建记录界面
     createrecord* ui_create_record;
-    std::vector<std::string> databases;
+    // Co1aSQL!
     QString prefix = "Co1aSQL > ";
+    // 当前选用的数据库
     std::string current_database = "";
+    // 当前查询表
     std::string current_table = "";
+    // 是否是多表查询
     bool multiple_select = 0;
+    // 当前选中的行和列
+    int clicked_row = -1;
+    int clicked_column = -1;
 };
 #endif // MAINWINDOW_H
