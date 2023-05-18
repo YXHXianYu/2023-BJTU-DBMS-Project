5b94985ea0a1feaae69e68bd5c79364c35afd51a
FuLiqi <1832271620@qq.com>
2023-05-19 02:14:40 +0800

    merge: from lost space

--- core/src/index/fhqtreapindex.cpp
+++ core/src/index/fhqtreapindex.cpp
@@ -2,56 +2,67 @@
 
 #include <iostream>
 
-FHQTreapIndex::FHQTreapIndex(const std::vector<std::unordered_map<std::string, std::any>>& records,
-                             const std::vector<std::pair<std::string, std::string>>& fields,
-                             const std::unordered_map<std::string, std::string>& field_map,
-                             const std::vector<std::string>& compare_key):
-    Index(records, fields, field_map, compare_key) {
+FHQTreapIndex::FHQTreapIndex(
+    const std::vector<std::unordered_map<std::string, std::any>>& records,
+    const std::vector<std::pair<std::string, std::string>>& fields,
+    const std::unordered_map<std::string, std::string>& field_map,
+    const std::vector<std::string>& compare_key)
+    : Index(records, fields, field_map, compare_key)
+{
     _records_ptr = nullptr;
     _field_map_ptr = nullptr;
     build(records, fields, field_map, compare_key);
 }
 
-FHQTreapIndex::~FHQTreapIndex() {
+FHQTreapIndex::~FHQTreapIndex()
+{
     // delete _records_ptr;
     _records_ptr = nullptr;
     // delete _field_map_ptr;
     _field_map_ptr = nullptr;
 }
 
-int FHQTreapIndex::build(const std::vector<std::unordered_map<std::string, std::any>>& records,
-                         const std::vector<std::pair<std::string, std::string>>& fields,
-                         const std::unordered_map<std::string, std::string>& field_map,
-                         const std::vector<std::string>& compare_key) {
-
+int FHQTreapIndex::build(
+    const std::vector<std::unordered_map<std::string, std::any>>& records,
+    const std::vector<std::pair<std::string, std::string>>& fields,
+    const std::unordered_map<std::string, std::string>& field_map,
+    const std::vector<std::string>& compare_key)
+{
     // ===== Records =====
     // delete _records_ptr;
     _records_ptr = nullptr;
-    _records_ptr = new std::vector<std::unordered_map<std::string, std::any>>(records);
+    _records_ptr =
+        new std::vector<std::unordered_map<std::string, std::any>>(records);
 
     // delete _field_map_ptr;
     _field_map_ptr = nullptr;
-    _field_map_ptr = new std::unordered_map<std::string, std::string>(field_map);
+    _field_map_ptr =
+        new std::unordered_map<std::string, std::string>(field_map);
 
     // ===== Compare Key =====
     // n^2 optimization could be used here
     _compare_key = compare_key;
-    for(const auto& field: fields) {
+    for (const auto& field : fields)
+    {
         int flag = 1;
-        for(const auto& key: _compare_key) {
-            if(field.first == key) {
+        for (const auto& key : _compare_key)
+        {
+            if (field.first == key)
+            {
                 flag = 0;
                 break;
             }
         }
-        if(flag) {
+        if (flag)
+        {
             _compare_key.push_back(field.first);
         }
     }
 
     // ===== Build Tree =====
     t = std::vector<Node>(records.size() + 2000);
-    for(int i = 0; i < records.size(); i++) {
+    for (int i = 0; i < records.size(); i++)
+    {
         insert(i);
     }
 
@@ -60,10 +71,14 @@ int FHQTreapIndex::build(const std::vector<std::unordered_map<std::string, std::
     return _state;
 }
 
-int FHQTreapIndex::query(const std::vector<std::tuple<std::string, std::string, int>>& conditions, std::vector<int>& result_indexes) {
+int FHQTreapIndex::query(
+    const std::vector<std::tuple<std::string, std::string, int>>& conditions,
+    std::vector<int>& result_indexes)
+{
     result_indexes.clear();
 
-    if(_state != 0) {
+    if (_state != 0)
+    {
         return kFailedIndexNotBuild;
     }
 
@@ -71,39 +86,50 @@ int FHQTreapIndex::query(const std::vector<std::tuple<std::string, std::string, 
     std::unordered_map<std::string, std::any> recordBasedOnConditions;
 
     // 构建 recordBasedOnConditions
-    for(int i = 0, cnt = 0; i < _compare_key.size() && cnt < conditions.size(); i++) {
+    for (int i = 0, cnt = 0; i < _compare_key.size() && cnt < conditions.size();
+         i++)
+    {
         int flag = false;
-        for(const auto& [name, value, type]: conditions) {
-            if(type != kEqualConditon) {
+        for (const auto& [name, value, type] : conditions)
+        {
+            if (type != kEqualConditon)
+            {
                 return kFailedConditionRelationNotSupport;
             }
-            if(name != _compare_key[i]) {
+            if (name != _compare_key[i])
+            {
                 continue;
             }
-            if(recordBasedOnConditions.count(name) > 0) {
+            if (recordBasedOnConditions.count(name) > 0)
+            {
                 return kFailedConditionDuplicate;
             }
-            recordBasedOnConditions.insert({name, ColasqlTool::GetAnyByTypeAndValue(_field_map_ptr->at(name), value)});
+            recordBasedOnConditions.insert(
+                {name, ColasqlTool::GetAnyByTypeAndValue(
+                           _field_map_ptr->at(name), value)});
             flag = true;
             break;
         }
-        if(flag) { // this key is in condition
-            cnt += 1;            
-        } else { // this key is not in condition
+        if (flag)
+        { // this key is in condition
+            cnt += 1;
+        }
+        else
+        { // this key is not in condition
             return kFailedIndexCouldnotSpeedup;
         }
     }
 
     // for(auto& [str, any]: recordBasedOnConditions) {
-    //     std::cout << str << ", " << ColasqlTool::AnyToString(any) << std::endl;
+    //     std::cout << str << ", " << ColasqlTool::AnyToString(any) <<
+    //     std::endl;
     // }
-
 
     // query
     int a, b, c;
     splitvR(rt, recordBasedOnConditions, a, b);
     splitv(b, recordBasedOnConditions, b, c, false);
-    
+
     output(b, result_indexes);
 
     rt = merge(a, merge(b, c));
@@ -115,16 +141,16 @@ int FHQTreapIndex::query(const std::vector<std::tuple<std::string, std::string, 
 #define LC (t[x].c[0])
 #define RC (t[x].c[1])
 
-void FHQTreapIndex::reset() {
+void FHQTreapIndex::reset()
+{
     rt = 0;
     tot = 0;
 }
 
-void FHQTreapIndex::pushup(int x) {
-    t[x].siz = t[LC].siz + t[RC].siz + 1;
-}
+void FHQTreapIndex::pushup(int x) { t[x].siz = t[LC].siz + t[RC].siz + 1; }
 
-int FHQTreapIndex::newNode(int va) {
+int FHQTreapIndex::newNode(int va)
+{
     tot++;
     t[tot].c[0] = 0;
     t[tot].c[1] = 0;
@@ -134,96 +160,132 @@ int FHQTreapIndex::newNode(int va) {
     return tot;
 }
 
-void FHQTreapIndex::splits(int x, int siz, int &l, int &r) {
-    if(x == 0) {
+void FHQTreapIndex::splits(int x, int siz, int& l, int& r)
+{
+    if (x == 0)
+    {
         l = r = 0;
         return;
     }
-    if(siz <= t[LC].siz) {
+    if (siz <= t[LC].siz)
+    {
         r = x;
         splits(LC, siz, l, LC);
-    } else {
+    }
+    else
+    {
         l = x;
-        splits(RC, siz-t[LC].siz-1, RC, r);
+        splits(RC, siz - t[LC].siz - 1, RC, r);
     }
     pushup(x);
 }
 
-void FHQTreapIndex::splitv(int x, int va, int &l, int &r, bool nullIsLess) {
-    if(x == 0) {
+void FHQTreapIndex::splitv(int x, int va, int& l, int& r, bool nullIsLess)
+{
+    if (x == 0)
+    {
         l = r = 0;
         return;
     }
-    if(compare(va, t[x].va, nullIsLess) < 0) {
+    if (compare(va, t[x].va, nullIsLess) < 0)
+    {
         r = x;
         splitv(LC, va, l, LC, nullIsLess);
-    } else {
+    }
+    else
+    {
         l = x;
         splitv(RC, va, RC, r, nullIsLess);
     }
     pushup(x);
 }
 
-void FHQTreapIndex::splitv(int x, const std::unordered_map<std::string, std::any>& va, int &l, int &r, bool nullIsLess) {
-    if(x == 0) {
+void FHQTreapIndex::splitv(int x,
+                           const std::unordered_map<std::string, std::any>& va,
+                           int& l, int& r, bool nullIsLess)
+{
+    if (x == 0)
+    {
         l = r = 0;
         return;
     }
-    if(compare(va, t[x].va, nullIsLess) < 0) {
+    if (compare(va, t[x].va, nullIsLess) < 0)
+    {
         r = x;
         splitv(LC, va, l, LC, nullIsLess);
-    } else {
+    }
+    else
+    {
         l = x;
         splitv(RC, va, RC, r, nullIsLess);
     }
     pushup(x);
 }
 
-void FHQTreapIndex::splitvR(int x, int va, int &l, int &r, bool nullIsLess) {
-    if(x == 0) {
+void FHQTreapIndex::splitvR(int x, int va, int& l, int& r, bool nullIsLess)
+{
+    if (x == 0)
+    {
         l = r = 0;
         return;
     }
-    if(compare(va, t[x].va, nullIsLess) <= 0) {
+    if (compare(va, t[x].va, nullIsLess) <= 0)
+    {
         r = x;
         splitvR(LC, va, l, LC, nullIsLess);
-    } else {
+    }
+    else
+    {
         l = x;
         splitvR(RC, va, RC, r, nullIsLess);
     }
     pushup(x);
 }
 
-void FHQTreapIndex::splitvR(int x, const std::unordered_map<std::string, std::any>& va, int &l, int &r, bool nullIsLess) {
-    if(x == 0) {
+void FHQTreapIndex::splitvR(int x,
+                            const std::unordered_map<std::string, std::any>& va,
+                            int& l, int& r, bool nullIsLess)
+{
+    if (x == 0)
+    {
         l = r = 0;
         return;
     }
-    if(compare(va, t[x].va, nullIsLess) <= 0) {
+    if (compare(va, t[x].va, nullIsLess) <= 0)
+    {
         r = x;
         splitvR(LC, va, l, LC, nullIsLess);
-    } else {
+    }
+    else
+    {
         l = x;
         splitvR(RC, va, RC, r, nullIsLess);
     }
     pushup(x);
 }
 
-int FHQTreapIndex::merge(int l, int r) {
-    if(l == 0 || r == 0) return l + r;
-    if(t[l].pos < t[r].pos) {
+int FHQTreapIndex::merge(int l, int r)
+{
+    if (l == 0 || r == 0)
+        return l + r;
+    if (t[l].pos < t[r].pos)
+    {
         t[l].c[1] = merge(t[l].c[1], r);
         pushup(l);
         return l;
-    } else {
+    }
+    else
+    {
         t[r].c[0] = merge(l, t[r].c[0]);
         pushup(r);
         return r;
     }
 }
 
-void FHQTreapIndex::output(int x, std::vector<int>& result) {
-    if(x == 0) return;
+void FHQTreapIndex::output(int x, std::vector<int>& result)
+{
+    if (x == 0)
+        return;
     result.push_back(t[x].va);
     output(LC, result);
     output(RC, result);
@@ -232,21 +294,23 @@ void FHQTreapIndex::output(int x, std::vector<int>& result) {
 #undef LC
 #undef RC
 
-
-void FHQTreapIndex::insert(int va) {
+void FHQTreapIndex::insert(int va)
+{
     int a, b;
     splitv(rt, va, a, b);
     rt = merge(merge(a, newNode(va)), b);
 }
 
-void FHQTreapIndex::erase(int va) {
+void FHQTreapIndex::erase(int va)
+{
     int a, b, c;
     splitv(rt, va, a, b);
     splitv(a, t[a].siz - 1, a, c);
     rt = merge(a, b);
 }
 
-int FHQTreapIndex::getRank(int va) {
+int FHQTreapIndex::getRank(int va)
+{
     int a, b, ans;
     splitv(rt, va - 1, a, b);
     ans = t[a].siz + 1;
@@ -254,7 +318,8 @@ int FHQTreapIndex::getRank(int va) {
     return ans;
 }
 
-int FHQTreapIndex::getNumber(int siz) {
+int FHQTreapIndex::getNumber(int siz)
+{
     int a, b, c, ans;
     splits(rt, siz, a, b);
     splits(a, siz - 1, a, c);
@@ -263,7 +328,8 @@ int FHQTreapIndex::getNumber(int siz) {
     return ans;
 }
 
-int FHQTreapIndex::getLower(int va) {
+int FHQTreapIndex::getLower(int va)
+{
     int a, b, c, ans;
     splitv(rt, va - 1, a, b);
     splits(a, t[a].siz - 1, a, c);
@@ -272,7 +338,8 @@ int FHQTreapIndex::getLower(int va) {
     return ans;
 }
 
-int FHQTreapIndex::getUpper(int va) {
+int FHQTreapIndex::getUpper(int va)
+{
     int a, b, c, ans;
     splitv(rt, va, a, b);
     splits(b, 1, b, c);
@@ -280,4 +347,3 @@ int FHQTreapIndex::getUpper(int va) {
     rt = merge(a, merge(b, c));
     return ans;
 }
-
